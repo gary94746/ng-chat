05dacd0dd8b29e06b01240cde8ec4adb7c25ce47
gary94746 <gary.109885@gmail.com>
2020-11-26 18:31:41 -0600

    feat: add delete button to chat

--- src/ng-chat/components/ng-chat-friends-list/ng-chat-friends-list.component.ts
+++ src/ng-chat/components/ng-chat-friends-list/ng-chat-friends-list.component.ts
@@ -1,149 +1,177 @@
-import { Component, Input, Output, EventEmitter, ViewEncapsulation, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+} from "@angular/core";
 
-import { Localization } from '../../core/localization';
-import { IChatOption } from '../../core/chat-option';
+import { Localization } from "../../core/localization";
+import { IChatOption } from "../../core/chat-option";
 import { ChatParticipantStatus } from "../../core/chat-participant-status.enum";
 import { IChatParticipant } from "../../core/chat-participant";
 import { User } from "../../core/user";
 import { Window } from "../../core/window";
 import { ParticipantResponse } from "../../core/participant-response";
 import { MessageCounter } from "../../core/message-counter";
-import { chatParticipantStatusDescriptor } from '../../core/chat-participant-status-descriptor';
+import { chatParticipantStatusDescriptor } from "../../core/chat-participant-status-descriptor";
 
 @Component({
-    selector: 'ng-chat-friends-list',
-    templateUrl: './ng-chat-friends-list.component.html',
-    styleUrls: ['./ng-chat-friends-list.component.css'],
-    encapsulation: ViewEncapsulation.None
+  selector: "ng-chat-friends-list",
+  templateUrl: "./ng-chat-friends-list.component.html",
+  styleUrls: ["./ng-chat-friends-list.component.css"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class NgChatFriendsListComponent implements OnChanges {
-    constructor() { }
+  constructor() {}
 
-    @Input()
-    public participants: IChatParticipant[];
+  @Input()
+  public participants: IChatParticipant[];
 
-    @Input()
-    public participantsResponse: ParticipantResponse[];
+  @Input()
+  public participantsResponse: ParticipantResponse[];
 
-    @Input()
-    public participantsInteractedWith: IChatParticipant[] = [];
+  @Input()
+  public participantsInteractedWith: IChatParticipant[] = [];
 
-    @Input()
-    public windows: Window[];
+  @Input()
+  public windows: Window[];
 
-    @Input()
-    public userId: any;
+  @Input()
+  public userId: any;
 
-    @Input()
-    public localization: Localization;
+  @Input()
+  public localization: Localization;
 
-    @Input()
-    public shouldDisplay: boolean;
+  @Input()
+  public shouldDisplay: boolean;
 
-    @Input()
-    public isCollapsed: boolean;
+  @Input()
+  public isCollapsed: boolean;
 
-    @Input()
-    public searchEnabled: boolean;
+  @Input()
+  public searchEnabled: boolean;
 
-    @Input()
-    public currentActiveOption: IChatOption | null;
+  @Input()
+  public currentActiveOption: IChatOption | null;
 
-    @Output()
-    public onParticipantClicked: EventEmitter<IChatParticipant> = new EventEmitter();
+  @Output()
+  public onParticipantClicked: EventEmitter<
+    IChatParticipant
+  > = new EventEmitter();
 
-    @Output()
-    public onOptionPromptCanceled: EventEmitter<any> = new EventEmitter();
+  @Output()
+  public onOptionPromptCanceled: EventEmitter<any> = new EventEmitter();
 
-    @Output()
-    public onOptionPromptConfirmed: EventEmitter<any> = new EventEmitter();
+  @Output()
+  public onOptionPromptConfirmed: EventEmitter<any> = new EventEmitter();
 
-    public selectedUsersFromFriendsList: User[] = [];
+  public selectedUsersFromFriendsList: User[] = [];
 
-    public searchInput: string = '';
+  public searchInput: string = "";
 
-    // Exposes enums and functions for the ng-template
-    public ChatParticipantStatus = ChatParticipantStatus;
-    public chatParticipantStatusDescriptor = chatParticipantStatusDescriptor;
+  // Exposes enums and functions for the ng-template
+  public ChatParticipantStatus = ChatParticipantStatus;
+  public chatParticipantStatusDescriptor = chatParticipantStatusDescriptor;
 
-    ngOnChanges(changes: SimpleChanges) {
-        if (this.currentActiveOption) {
-            const currentOptionTriggeredBy = this.currentActiveOption && this.currentActiveOption.chattingTo.participant.id;
-            const isActivatedUserInSelectedList = (this.selectedUsersFromFriendsList.filter(item => item.id == currentOptionTriggeredBy)).length > 0;
+  ngOnChanges(changes: SimpleChanges) {
+    if (this.currentActiveOption) {
+      const currentOptionTriggeredBy =
+        this.currentActiveOption &&
+        this.currentActiveOption.chattingTo.participant.id;
+      const isActivatedUserInSelectedList =
+        this.selectedUsersFromFriendsList.filter(
+          (item) => item.id == currentOptionTriggeredBy
+        ).length > 0;
 
-            if (!isActivatedUserInSelectedList) {
-                this.selectedUsersFromFriendsList = this.selectedUsersFromFriendsList.concat(this.currentActiveOption.chattingTo.participant as User);
-            }
-        }
+      if (!isActivatedUserInSelectedList) {
+        this.selectedUsersFromFriendsList = this.selectedUsersFromFriendsList.concat(
+          this.currentActiveOption.chattingTo.participant as User
+        );
+      }
+    }
+  }
+
+  deleteParticipant(userId: any) {
+    this.participants = this.participants.filter((x) => x.id != userId);
+    this.filteredParticipants;
+  }
+
+  get filteredParticipants(): IChatParticipant[] {
+    if (this.searchInput.length > 0) {
+      // Searches in the friend list by the inputted search string
+      return this.participants.filter((x) =>
+        x.displayName.toUpperCase().includes(this.searchInput.toUpperCase())
+      );
     }
 
-    get filteredParticipants(): IChatParticipant[]
-    {
-        if (this.searchInput.length > 0){
-            // Searches in the friend list by the inputted search string
-            return this.participants.filter(x => x.displayName.toUpperCase().includes(this.searchInput.toUpperCase()));
-        }
+    return this.participants;
+  }
 
-        return this.participants;
+  isUserSelectedFromFriendsList(user: User): boolean {
+    return (
+      this.selectedUsersFromFriendsList.filter((item) => item.id == user.id)
+        .length > 0
+    );
+  }
+
+  unreadMessagesTotalByParticipant(participant: IChatParticipant): string {
+    let openedWindow = this.windows.find(
+      (x) => x.participant.id == participant.id
+    );
+
+    if (openedWindow) {
+      return MessageCounter.unreadMessagesTotal(openedWindow, this.userId);
+    } else {
+      let totalUnreadMessages = this.participantsResponse
+        .filter(
+          (x) =>
+            x.participant.id == participant.id &&
+            !this.participantsInteractedWith.find(
+              (u) => u.id == participant.id
+            ) &&
+            x.metadata &&
+            x.metadata.totalUnreadMessages > 0
+        )
+        .map((participantResponse) => {
+          return participantResponse.metadata.totalUnreadMessages;
+        })[0];
+
+      return MessageCounter.formatUnreadMessagesTotal(totalUnreadMessages);
     }
+  }
 
-    isUserSelectedFromFriendsList(user: User) : boolean
-    {
-        return (this.selectedUsersFromFriendsList.filter(item => item.id == user.id)).length > 0
+  cleanUpUserSelection = () => (this.selectedUsersFromFriendsList = []);
+
+  // Toggle friends list visibility
+  onChatTitleClicked(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  onFriendsListCheckboxChange(selectedUser: User, isChecked: boolean): void {
+    if (isChecked) {
+      this.selectedUsersFromFriendsList.push(selectedUser);
+    } else {
+      this.selectedUsersFromFriendsList.splice(
+        this.selectedUsersFromFriendsList.indexOf(selectedUser),
+        1
+      );
     }
+  }
 
-    unreadMessagesTotalByParticipant(participant: IChatParticipant): string
-    {
-        let openedWindow = this.windows.find(x => x.participant.id == participant.id);
+  onUserClick(clickedUser: User): void {
+    this.onParticipantClicked.emit(clickedUser);
+  }
 
-        if (openedWindow){
-            return MessageCounter.unreadMessagesTotal(openedWindow, this.userId);
-        }
-        else
-        {
-            let totalUnreadMessages = this.participantsResponse
-                .filter(x => x.participant.id == participant.id && !this.participantsInteractedWith.find(u => u.id == participant.id) && x.metadata && x.metadata.totalUnreadMessages > 0)
-                .map((participantResponse) => {
-                    return participantResponse.metadata.totalUnreadMessages
-                })[0];
+  onFriendsListActionCancelClicked(): void {
+    this.onOptionPromptCanceled.emit();
+    this.cleanUpUserSelection();
+  }
 
-            return MessageCounter.formatUnreadMessagesTotal(totalUnreadMessages);
-        }
-    }
-
-    cleanUpUserSelection = () => this.selectedUsersFromFriendsList = [];
-
-    // Toggle friends list visibility
-    onChatTitleClicked(): void
-    {
-        this.isCollapsed = !this.isCollapsed;
-    }
-
-    onFriendsListCheckboxChange(selectedUser: User, isChecked: boolean): void
-    {
-        if(isChecked) {
-            this.selectedUsersFromFriendsList.push(selectedUser);
-        } 
-        else 
-        {
-            this.selectedUsersFromFriendsList.splice(this.selectedUsersFromFriendsList.indexOf(selectedUser), 1);
-        }
-    }
-
-    onUserClick(clickedUser: User): void
-    {
-        this.onParticipantClicked.emit(clickedUser);
-    }
-
-    onFriendsListActionCancelClicked(): void
-    {
-        this.onOptionPromptCanceled.emit();
-        this.cleanUpUserSelection();
-    }
-
-    onFriendsListActionConfirmClicked() : void
-    {
-        this.onOptionPromptConfirmed.emit(this.selectedUsersFromFriendsList);
-        this.cleanUpUserSelection();
-    }
+  onFriendsListActionConfirmClicked(): void {
+    this.onOptionPromptConfirmed.emit(this.selectedUsersFromFriendsList);
+    this.cleanUpUserSelection();
+  }
 }

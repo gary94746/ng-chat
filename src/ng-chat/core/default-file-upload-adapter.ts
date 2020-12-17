36d321332ba5d8b12d253e5c7eb7f8c0c21ca122
edgar <edgar.109885@gmail.com>
2020-12-17 09:31:52 -0600

    refactor: file upload adapter

--- src/ng-chat/core/default-file-upload-adapter.ts
+++ src/ng-chat/core/default-file-upload-adapter.ts
@@ -1,51 +1,61 @@
-import { IFileUploadAdapter } from './file-upload-adapter';
-import { HttpClient, HttpRequest, HttpEventType, HttpResponse, HttpHeaders } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs';
-import { User } from './user';
-import { Message } from './message';
+import { IFileUploadAdapter } from "./file-upload-adapter";
+import {
+  HttpClient,
+  HttpRequest,
+  HttpEventType,
+  HttpResponse,
+  HttpHeaders,
+} from "@angular/common/http";
+import { Observable, Subject } from "rxjs";
+import { User } from "./user";
+import { Message } from "./message";
 
-export class DefaultFileUploadAdapter implements IFileUploadAdapter
-{
-    /**
-     * @summary Basic file upload adapter implementation for HTTP request form file consumption
-     * @param _serverEndpointUrl The API endpoint full qualified address that will receive a form file to process and return the metadata.
-     */
-    constructor(private _serverEndpointUrl: string, private _http: HttpClient) {
-    }
+export class DefaultFileUploadAdapter implements IFileUploadAdapter {
+  /**
+   * @summary Basic file upload adapter implementation for HTTP request form file consumption
+   * @param _serverEndpointUrl The API endpoint full qualified address that will receive a form file to process and return the metadata.
+   */
+  constructor(private _serverEndpointUrl: string, private _http: HttpClient) {}
 
-    uploadFile(file: File, participantId: any): Observable<Message> {
-        const formData: FormData = new FormData();
+  uploadFile(
+    file: File,
+    participantId: any
+  ): Observable<{ success: boolean; url: string }> {
+    const formData: FormData = new FormData();
 
-        //formData.append('ng-chat-sender-userid', currentUserId);
-        formData.append('ng-chat-participant-id', participantId);
-        formData.append('file', file, file.name);
+    //formData.append('ng-chat-sender-userid', currentUserId);
+    formData.append("ng-chat-participant-id", participantId);
+    formData.append("file", file, file.name);
 
-        return this._http.post<Message>(this._serverEndpointUrl, formData);
+    return this._http.post<{ success: boolean; url: string }>(
+      this._serverEndpointUrl + participantId,
+      formData
+    );
 
-        // TODO: Leaving this if we want to track upload progress in detail in the future. Might need a different Subject generic type wrapper
-        // const fileRequest = new HttpRequest('POST', this._serverEndpointUrl, formData, {
-        //     reportProgress: true
-        // });
+    // TODO: Leaving this if we want to track upload progress in detail in the future. Might need a different Subject generic type wrapper
+    // const fileRequest = new HttpRequest('POST', this._serverEndpointUrl, formData, {
+    //     reportProgress: true
+    // });
 
-        // const uploadProgress = new Subject<number>();
-        // const uploadStatus = uploadProgress.asObservable();
+    // const uploadProgress = new Subject<number>();
+    // const uploadStatus = uploadProgress.asObservable();
 
-        //const responsePromise = new Subject<Message>();
+    //const responsePromise = new Subject<Message>();
 
-        // this._http
-        //     .request(fileRequest)
-        //     .subscribe(event => {
-        //         // if (event.type == HttpEventType.UploadProgress)
-        //         // {
-        //         //     const percentDone = Math.round(100 * event.loaded / event.total);
+    // this._http
+    //     .request(fileRequest)
+    //     .subscribe(event => {
+    //         // if (event.type == HttpEventType.UploadProgress)
+    //         // {
+    //         //     const percentDone = Math.round(100 * event.loaded / event.total);
 
-        //         //     uploadProgress.next(percentDone);
-        //         // }
-        //         // else if (event instanceof HttpResponse)
-        //         // {
-                    
-        //         //     uploadProgress.complete();
-        //         // }
-        //     });
-    }
+    //         //     uploadProgress.next(percentDone);
+    //         // }
+    //         // else if (event instanceof HttpResponse)
+    //         // {
+
+    //         //     uploadProgress.complete();
+    //         // }
+    //     });
+  }
 }
--- src/ng-chat/core/file-upload-adapter.ts
+++ src/ng-chat/core/file-upload-adapter.ts
@@ -1,8 +1,10 @@
-import { Observable } from 'rxjs';
-import { User } from './user';
-import { Message } from './message';
+import { Observable } from "rxjs";
+import { User } from "./user";
+import { Message } from "./message";
 
-export interface IFileUploadAdapter
-{
-    uploadFile(file: File, participantId: any): Observable<Message>;
+export interface IFileUploadAdapter {
+  uploadFile(
+    file: File,
+    participantId: any
+  ): Observable<{ success: boolean; url: string }>;
 }
